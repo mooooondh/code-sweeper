a32750558c0183f68fe2d751a613770fe5a63c03
mooooondh <mdh9514@gmail.com>
2024-06-10 00:35:31 +0900

    update : 프롬프트 업데이트

--- src/app/api/chatAi/route.ts
+++ src/app/api/chatAi/route.ts
@@ -11,15 +11,14 @@ export async function POST(req: NextRequest) {
 	const data = await req.json();
 
 	const inputText = `
-  아래 입력된 글이 ${data.language}코드인지 확인해줘.
-  만약 ${data.language}코드가 아니라면, "false"라는 글자만 출력해줘.
-  만약 ${data.language}코드가 맞다면, 아래 입력된 ${data.language}코드를 클린 코드로 수정해주고, 가독성을 높여줘.
-  가능하다면, 변수명, 함수명 등의 이름도 이해하기 쉽게 바꿔줘.
-	작성된 코드의 동작을 수정하지는 말아줘.
-  수정된 코드는 markdown으로 작성해줘.
-  수정 전 코드는 출력하지 말아줘.
+Check if the text below is in ${data.language} code.
+If it is not in ${data.language} code, output only the word "false".
+If it is in ${data.language} code, enhance the readability of the ${data.language} code below.
+The revised code must produce the same result as the original code.
+Write the revised code in markdown.
+Do not output the original code.
 
-  ${data.inputCode}
+${data.inputCode}
   `;
 
 	const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
